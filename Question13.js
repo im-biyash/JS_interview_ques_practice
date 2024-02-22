90f2d4baaa65514ab7871a6077f13a5046017d70
biyash <bsstha21@gmail.com>
2024-02-22 15:45:23 +0545

    let var cosnt

---
+++ Question13.js
@@ -0,0 +1,48 @@
+
+// Explain about var let and const
+// scope
+
+// var is functional scope
+// let and var are block scope
+
+// var name = "biaysh";
+// name = "shrestha";
+// console.log(name);
+
+// let name2 = "prijal";
+// name2 = "shrestha";
+// console.log(name2);
+// function modJs(love){
+//     if(love){
+//         let name = "Biyash";
+//         const sname = "shrestha";
+//     }
+//     console.log("my name is" + name + "" + sname); 
+//     //  here let and var must e defeind in block scope
+// // }
+// modJs(true);
+// function modJs(love){
+//     if(love){
+//         var name = "Biyash";
+//         var sname = "shrestha";
+//     }
+//     console.log("my name is" + name + "" + sname);
+// }
+// modJs(true);
+
+// var bestcoder = "hitesh";
+// if(true){
+//     var bestcoder = "Harry";
+//     console.log(bestcoder);
+// }
+// console.log(bestcoder)   
+// output:Harry
+
+let bestcoder = "hitesh";
+if(true){
+    let bestcoder = "Harry";
+    console.log(bestcoder);
+}
+console.log(bestcoder)  
+// output:harry hitesh
+// outside block 
