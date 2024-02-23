6713ccf34294571265481bae028fe8fe2c04f0b8
biyash <bsstha21@gmail.com>
2024-02-23 16:34:23 +0545

    spread and rest operators

---
+++ Question15.js
@@ -0,0 +1,13 @@
+// Example of rest and spread operator
+// spread:used to expand an array or objects
+// rest: used to extract elements from an array
+const arr = [1, 2, 3, 4, 5];
+const arr1 = [...arr,6,7,8,9];  /*used to copy the arrray*/
+console.log(arr1);
+
+// compose and merge using spread
+const oldNumber = [1, 2, 3];
+const newNumber = [4, 5, 6];
+const allNumber = [100,...oldNumber, ...newNumber,200];
+console.log(allNumber);
+
