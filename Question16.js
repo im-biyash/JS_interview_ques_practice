99a9c037692474980b54ce8711cbbea15cc538aa
biyash <bsstha21@gmail.com>
2024-02-24 18:02:14 +0545

    closures

--- Question16.js
+++ Question16.js
@@ -1,12 +1,25 @@
 // closures
 // a closure is a function that has access to the parent scope, even after the parent function has returned.
-function init() {
-    let name = "Mozilla"; // name is a local variable created by init
-    function displayName() {
-      // displayName() is the inner function, that forms the closure
-      console.log(name); // use variable declared in the parent function
-    }
-    displayName();
+// function init() {
+//   let name = "Mozilla"; // name is a local variable created by init
+//   function displayName() {
+//     // displayName() is the inner function, that forms the closure
+//     console.log(name); // use variable declared in the parent function
+//   }
+//   displayName();
+// }
+// init();
+
+message = "good global";
+function hello1() {
+  message = "good morning";
+
+  function hello2() {
+    console.log("hello2" + message);
   }
-  init();
-  
+  message = "biyash"
+  return hello2;
+}
+let c = hello1();
+c();
+// output will be 
