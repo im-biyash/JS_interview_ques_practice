37838aa95540122593db692794eb67d7275f0002
biyash <bsstha21@gmail.com>
2024-02-26 17:41:47 +0545

    closures

--- Question17.js
+++ Question17.js
@@ -1,12 +1,25 @@
-for(var i = 0;i<3;i++){
-   setTimeout(() =>{
-        console.log(i); 
-    }
-    ),1000}
+// for(var i = 0;i<3;i++){
+//    setTimeout(() =>{
+//         console.log(i);
+//     }
+//     ),1000}
 
-    // output : 3 3 3 because of var var doenst have block scope it have functin scope
-    for(let i = 0;i<3;i++){
-        setTimeout(() =>{
-             console.log(i); 
-         }
-         ),1000}
+//     // output : 3 3 3 because of var var doenst have block scope it have functin scope
+//     for(let i = 0;i<3;i++){
+//         setTimeout(() =>{
+//              console.log(i);
+//          }
+//          ),1000}
+
+//  we can also solve it using var using closure
+for (let i = 0; i < 3; i++) {
+
+    function inner(i){
+        setTimeout(() => {
+          console.log(i);
+        }),
+          1000;
+    }
+    inner(i);  
+}
+// due to closures output is 0 1 2 
