b9e25057c1f819aca688f721393b786f85f4f0ac
biyash <bsstha21@gmail.com>
2024-03-09 18:45:59 +0545

    objects

--- Question19.js
+++ Question19.js
@@ -10,20 +10,31 @@
 // create function multiplyByTwo(obj) that multiplies all numeric property values of num
 // nums by 2
 
-let nums = {
-    a: 10,
-    b: 20,
-    title: "My nums"
-}
+// let nums = {
+//     a: 10,
+//     b: 20,
+//     title: "My nums"
+// }
 
-multiplyByTwo(nums);
+// multiplyByTwo(nums);
 
-function multiplyByTwo(obj){
-    for(key in obj){
-      if( typeof obj[key]=== "number"){
-        obj[key] *= 2;
+// function multiplyByTwo(obj){
+//     for(key in obj){
+//       if( typeof obj[key]=== "number"){
+//         obj[key] *= 2;
 
-      }
-    }
-}
-console.log(nums);
+//       }
+//     }
+// }
+// console.log(nums);
+const user = {
+    name: 'biyash',
+    id :2,
+   
+};
+
+const strobj = JSON.stringify(user);
+console.log(strobj);       
+
+const parsobj  = JSON.parse(strobj);   
+console.log(parsobj);
